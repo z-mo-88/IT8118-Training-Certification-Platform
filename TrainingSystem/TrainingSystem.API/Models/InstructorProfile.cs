51242dd1f9ce8f75e98acdec29d17ad24914fa1e
DanaK5 <202300513@student.polytechnic.bh>
2026-04-05 14:17:11 +0300

    Update InstructorProfile.cs

--- TrainingSystem/TrainingSystem.API/Models/InstructorProfile.cs
+++ TrainingSystem/TrainingSystem.API/Models/InstructorProfile.cs
@@ -7,13 +7,15 @@ namespace TrainingSystem.API.Models;
 public partial class InstructorProfile
 {
     [Key]
-    public int InstructorExpertiseId { get; set; }
-
-    [Required]
-    public int ExpertiseId { get; set; }
-
-    [Required]
     public int UserId { get; set; }
+
+    [Required]
+    [StringLength(1000)]
+    public string Bio { get; set; } = null!;
+
+    [Required]
+    [StringLength(1000)]
+    public string Notes { get; set; } = null!;
 
     public virtual User User { get; set; } = null!;
 }
