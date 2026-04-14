6f58b1aa49277f64a543e338cfd422ebea958632
Maryam <maryam.alshehabi@gmail.com>
2026-04-15 01:53:16 +0300

    design

---
+++ TrainingSystem/TrainingSystem.API/Migrations/20260414191908_Fix11.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingSystem.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
