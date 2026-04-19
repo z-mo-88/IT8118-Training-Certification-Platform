4e6ac19c4cb9a4e66f55aed32443e9acf162b957
DanaK5 <202300513@student.polytechnic.bh>
2026-04-19 14:30:27 +0300

    Create InstructorDisplayViewModel.cs

---
+++ TrainingSystem/TrainingSystem.MVC/Models/InstructorDisplayViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TrainingSystem.MVC.Models
+{
+    public class InstructorDisplayViewModel
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+
+        public bool IsActive { get; set; }
+
+        public string Bio { get; set; } = "";
+        public string Notes { get; set; } = "";
+
+        public List<string> ExpertiseNames { get; set; } = new();
+        public List<string> AvailabilityText { get; set; } = new();
+    }
+}
