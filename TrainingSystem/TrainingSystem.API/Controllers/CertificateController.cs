918efce84de1ed61adcee094fcca5fc68b60dad9
DanaK5 <202300513@student.polytechnic.bh>
2026-05-18 23:04:31 +0300

    Update CertificateController.cs

--- TrainingSystem/TrainingSystem.API/Controllers/CertificateController.cs
+++ TrainingSystem/TrainingSystem.API/Controllers/CertificateController.cs
@@ -17,7 +17,7 @@ namespace TrainingSystem.API.Controllers
         }
 
         [HttpGet]
-        [Authorize]
+        [Authorize(Roles = "1")]
         public async Task<IActionResult> GetCertificates()
         {
             var certificates = await _context.Certificates
